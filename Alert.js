5ae8827bc9861b353ca8b51d06f5e1b573084b53
Huzaifa-67 <huzaifa.altaf04@gmail.com>
2025-02-18 17:46:01 +0500

    1 assignment

--- Alert.js
+++ Alert.js
@@ -11,4 +11,42 @@ alert("JavaScript Alert\n\nWelcome to JS Land...\nHappy Coding!")
 alert("JavaScript Alert\n\nWelcome to JS Land...")
 alert("JavaScript Alert\n\nHappy Coding!\nPrevent this page from creating additional dialogs.")
 alert("JavaScript Alert\n\nHello... I can run JS through my web browser's console")
-alert("JavaScript Alert\n\nGoodbye...")
+
+let username = "Huzaifa Altaf"
+let Age ="20 years old"
+let course = "Modern Web Application Development"
+let Afood = "PIZZA"
+let Bfood = "PIZZ"
+let Cfood = "PIZ"
+let Dfood = "PI"
+let Efood = "P"
+let email = "My email address is Huzaifa.altaf04@gmail.com"
+let book = "I am trying to learn from the Book A smarter way to learn JavaScript"
+let message = "Yah! I can write HTML content through JavaScript"
+let lastmessage = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+let visitor = "You have visited this site 3 times"
+let birthyear ="My birth year is 2004 "
+let birthyearr = "Data type of my declared variable is number"
+let Visitorname = "John Doe"
+let ProductTitle = "5 T-shirt"
+let Quantity = "5"
+let last = "(s) on XYZ Clothing store"
+
+
+alert(username);
+alert(Age);
+alert(course);
+alert(Afood + "\n" + Bfood + "\n" + Cfood + "\n" + Dfood + "\n" + Efood)
+alert(email);
+alert(book);
+document.write(message + "<br>");
+alert(lastmessage);
+alert(Age)
+alert(visitor);
+document.write(" " + birthyear + "<br>");
+document.write(" " + birthyearr + "<br>");
+
+
+document.write(Visitorname + " " + "Ordered" +  " " + ProductTitle + " " + Quantity + " " + last);
+
+  
